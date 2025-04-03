7e15e38acbaf0752b31ad6ece18cbaaadc3e119a
Brando <fong.m.brandon97@gmail.com>
2025-04-03 13:26:16 -0700

    can handle requests on separate queue

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,18 @@ extern "C" {
 }
 
 using namespace BF::Net;
+using namespace BF;
 using namespace std;
+
+/**
+ * holds all incoming envelopes from open socket
+ *
+ * all envelopes are retained, so these should be released
+ * once popped from queue
+ */
+Atomic<Queue<Envelope *>> _incomingRequests;
+BFThreadAsyncID _tidRequestQueue = NULL;
+BFLock _queueSema;
 
 void __LogCallbackBFNet(const char * buf) { \
 	cout << "bfnet: " << buf << endl;
@@ -24,12 +35,46 @@ void __NewConnection(Connection * sc) {
 }
 
 void __PacketReceive(Envelope * envelope) {
-	cout << "envelope: " << (const char *) envelope->data()->buffer() << endl;
+	_incomingRequests.lock();
+
+	BFRetain(envelope);
+	_incomingRequests.unsafeget().push(envelope);
+
+	_incomingRequests.unlock();
+	BFLockRelease(&_queueSema);
+}
+
+void _IncomingRequestsWorkerThread(void * in) {
+	while (!BFThreadAsyncIsCanceled(_tidRequestQueue)) {
+		if (_incomingRequests.get().empty()) { 
+			BFLockWait(&_queueSema);
+		} else {
+			_incomingRequests.lock();
+
+			// get first item from the queue
+			Envelope * envelope = _incomingRequests.unsafeget().front();
+
+			// pop off
+			_incomingRequests.unsafeget().pop();
+	
+			cout << "envelope: " << (const char *) envelope->data()->buffer() << endl;
+
+			// release memory
+			BFRelease(envelope);
+
+			_incomingRequests.unlock();
+		}
+	}
+
 }
 
 int main() {
 	int error = 0;
 	Log::SetCallback(__LogCallbackBFNet);
+
+	BFLockCreate(&_queueSema);
+	_tidRequestQueue = BFThreadAsync(_IncomingRequestsWorkerThread, NULL);
+
 	Socket * skt = Socket::create(SOCKET_MODE_SERVER, "0.0.0.0", 8080, &error);
 	if (!error) {
 		skt->setInStreamCallback(__PacketReceive);
@@ -45,6 +90,12 @@ int main() {
 	}
 
 	BFRelease(skt);
+
+	BFThreadAsyncCancel(_tidRequestQueue);
+	BFThreadAsyncWait(_tidRequestQueue);
+	BFThreadAsyncDestroy(_tidRequestQueue);
+
+	BFLockDestroy(&_queueSema);
 
 	return error;
 }
